e497abd4426a0d3b3b80feb6b12dff10e683928e
Hao Wang <wang.hao.chnbj@gmail.com>
2016-08-06 23:03:01 -0400

    initial commit

---
+++ AppFriendsCore.framework/Headers/AppFriendsCore.h
@@ -0,0 +1,19 @@
+//
+//  AppFriendsCore.h
+//  AppFriendsCore
+//
+//  Created by HAO WANG on 8/6/16.
+//  Copyright © 2016 Bilal Kheir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppFriendsCore.
+FOUNDATION_EXPORT double AppFriendsCoreVersionNumber;
+
+//! Project version string for AppFriendsCore.
+FOUNDATION_EXPORT const unsigned char AppFriendsCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppFriendsCore/PublicHeader.h>
+
+
