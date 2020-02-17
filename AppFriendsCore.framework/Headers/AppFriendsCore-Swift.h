e0f4c9b1fa6f17c94313a4ccf605a6bc2bdc96e8
wanghao <hao.wang@hacknocraft.com>
2020-02-16 22:09:27 -0500

    Use BUILD_LIBRARY_FOR_DISTRIBUTION

--- AppFriendsCore.framework/Headers/AppFriendsCore-Swift.h
+++ AppFriendsCore.framework/Headers/AppFriendsCore-Swift.h
@@ -238,7 +238,7 @@ SWIFT_PROTOCOL("_TtP14AppFriendsCore20AFEncryptionDelegate_")
 - (void)decryptWithEncryptedTexts:(NSArray<NSString *> * _Nonnull)encryptedTexts forDialog:(NSString * _Nonnull)dialogID completion:(void (^ _Nullable)(NSArray<NSString *> * _Nullable, NSError * _Nullable))completion;
 @end
 
-typedef SWIFT_ENUM(NSInteger, AFError, closed) {
+typedef SWIFT_ENUM(NSInteger, AFError, open) {
   AFErrorUnknownError = 90000,
   AFErrorInvalidParams = 90001,
   AFErrorSdkNotInitialized = 90002,
