f6916334dbbe6e5fb76cc1e5acb6895417a36679
wanghao <hao.wang@hacknocraft.com>
2021-10-22 11:44:04 -0400

    commit release 3.6.0, xcode13 support.

--- AppFriendsCore.framework/Headers/AppFriendsCore-Swift.h
+++ AppFriendsCore.framework/Headers/AppFriendsCore-Swift.h
@@ -248,7 +248,7 @@ SWIFT_PROTOCOL("_TtP14AppFriendsCore20AFEncryptionDelegate_")
 - (void)decryptWithEncryptedTexts:(NSArray<NSString *> * _Nonnull)encryptedTexts forDialog:(NSString * _Nonnull)dialogID completion:(void (^ _Nullable)(NSArray<NSString *> * _Nullable, NSError * _Nullable))completion;
 @end
 
-typedef SWIFT_ENUM(NSInteger, AFError, closed) {
+typedef SWIFT_ENUM(NSInteger, AFError, open) {
   AFErrorUnknownError = 90000,
   AFErrorInvalidParams = 90001,
   AFErrorSdkNotInitialized = 90002,
@@ -602,7 +602,7 @@ SWIFT_PROTOCOL("_TtP14AppFriendsCore20AFEncryptionDelegate_")
 - (void)decryptWithEncryptedTexts:(NSArray<NSString *> * _Nonnull)encryptedTexts forDialog:(NSString * _Nonnull)dialogID completion:(void (^ _Nullable)(NSArray<NSString *> * _Nullable, NSError * _Nullable))completion;
 @end
 
-typedef SWIFT_ENUM(NSInteger, AFError, closed) {
+typedef SWIFT_ENUM(NSInteger, AFError, open) {
   AFErrorUnknownError = 90000,
   AFErrorInvalidParams = 90001,
   AFErrorSdkNotInitialized = 90002,
